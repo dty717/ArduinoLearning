26010c70f40e35bd6652960de3aabbfe20a63428
1875167603 <1875167603@qq.com>
2018-11-16 06:53:44 +0800

    first init

---
+++ demo/wifi/Arduino_CMD.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Arduino\arduino-cli" compile --fqbn arduino:avr:uno "C:\Users\xqy\Desktop\github\dty717\ArduinoLearning\demo\wifi"
+"C:\Program Files\Arduino\arduino-cli" upload -p COM14 --fqbn arduino:avr:uno "C:\Users\xqy\Desktop\github\dty717\ArduinoLearning\demo\wifi"
+pause
